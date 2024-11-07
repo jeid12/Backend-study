17d3f914f94cd5d021ff5a69c2deeba8fe29b7a3
jeid12 <113944390+jeid12@users.noreply.github.com>
2024-11-07 17:33:17 +0000

    added ejs

--- index.js
+++ index.js
@@ -2,9 +2,14 @@
 const express = require("express");
 const app = express();
 const authorRouter = require("./routes/authorRouter");
+const path = require("node:path");
 
 
 app.use("/authors", authorRouter);
+
+//ejs
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 
 const PORT = 3000;
