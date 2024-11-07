f7628f2b1eaf53f7d8358738eb4c5abe88d2c95a
jeid12 <113944390+jeid12@users.noreply.github.com>
2024-11-07 17:59:12 +0000

    added

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.use("/authors", authorRouter);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.get("/", (req, res) => {
+  res.render("index", { message: "EJS rocks!" });
+});
+
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
